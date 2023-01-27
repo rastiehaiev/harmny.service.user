670c1e85b65f6a970aa0b4de17a851635ad26566
Roman Rastiehaiev <roman.rastiehaiev@gmail.com>
2023-01-27 21:54:17 +0000

    [HARMNY-T-23] Run build job only from master.

--- .space.kts
+++ .space.kts
@@ -1,4 +1,11 @@
 job("Build") {
+    startOn {
+        gitPush {
+            branchFilter {
+                +"refs/heads/main"
+            }
+        }
+    }
     container(displayName = "Build", image = "gradle:6.3-jdk11") {
         kotlinScript { api ->
             api.gradlew("build")
